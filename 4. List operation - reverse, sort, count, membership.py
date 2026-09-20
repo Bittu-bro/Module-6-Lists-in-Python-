35a04b9a0993a4c6010ad3f6d5293e708dc7ff33
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-20 17:55:11 +0530

    feat: add reverse, sort, count and membership examples

--- 4. List operation - reverse, sort, count, membership.py
+++ 4. List operation - reverse, sort, count, membership.py
@@ -1,4 +1,45 @@
-a = "hello world,"
-b = "My name is Bittu,"
-c = "I am learning git."
-print(f"{a}\n{b}\n{c}")
+#reversr
+#days_of_weak = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+#print("Days of weak:",days_of_weak)
+#days_of_weak.reverse()
+#print("Reverse days of weak :",days_of_weak)
+
+
+
+
+#sort
+#nums = [4,5,1,0,7,1,4,5,7,9,0,5,3,3]
+#print("Our normal number is:",nums)
+#nums.sort()
+#print("Now, this is the ascending order of number:",nums)
+#nums.sort(reverse=True)
+#print("After that, this is the decending order of number:",nums)
+
+
+
+
+#count
+#nums = [2,4,7,8,9,0,0,9,4,6,4,2,5,9,0,3,5,]
+#print(f"Our lists is \'{nums}\'")
+#occurrences = nums.count(int(input(f"Elect the number from the uper lists\" ")))
+#print(f"Total number of occurrences is \'{occurrences}\' in the lists.")
+
+
+
+
+#menbership("in" or "not in")
+
+### in
+#days_of_weak = ["mon",  "wed", "thu", "sat", "tue", "wed", "fri", "tue", "wed", "thu", "sat", "sun"]
+#fun1 = len(days_of_weak)
+#print(f"We have total \'{fun1}\' holidays in this month.")
+#fun2 = (input(f" Please select day on which you like to take leave.") in days_of_weak)
+#print(f"It is \'{fun2}\' that the day you selected is avilable.")
+
+
+### not in
+#working_days_of_weak = ["mon", "thu", "sat", "sun"]
+#fun1 = len(working_days_of_weak)
+#print(f"We have total \'{fun1}\' workdays in this weak.")
+#fun2 = (input(f" Please select day on which you like to take leave.") not in working_days_of_weak)
+#print(f"It is \'{fun2}\',That the day you selected is avilable.")
