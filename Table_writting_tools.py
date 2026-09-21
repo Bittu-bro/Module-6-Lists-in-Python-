7c73ccd863f36ba2f8222ea2f3747b96a11ec23f
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-21 17:05:40 +0530

    For table

---
+++ Table_writting_tools.py
@@ -0,0 +1,16 @@
+#if you want want decimal table then use fun1
+                   #fun1 
+#num = float(input("Enter your \'Decimal\' number: "))
+
+
+
+
+#if you want want integer table then use fun2
+                   #fun2 = 
+num = int(input("Enter your \'Integer\' number: "))
+
+
+
+
+
+print(f"Here is the your table \n\t{num * 1}\n\t{num * 2}\n\t{num * 3}\n\t{num * 4}\n\t{num * 5}\n\t{num * 6}\n\t{num * 7}\n\t{num * 8}\n\t{num * 9}\n\t{num * 10}")
